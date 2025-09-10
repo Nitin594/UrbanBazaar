eab1b7cea63e89dfb16e83ac41c41c95ca82c7cd
Nitin594 <nitinmaurya594@gmail.com>
2025-09-10 19:22:36 +0530

    Added user register functionality

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,2 +1,60 @@
-export const registerController = () => {};
+import userModel from "../models/userModel.js";
+import { ApiError } from "../utils/apiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { hashedPassword } from "../helpers/authHelper.js";
 
+export const registerController = asyncHandler(async (req, res) => {
+  try {
+    const { name, email, password, phone, address } = req.body;
+
+    //validation
+    if (!name) {
+      throw new ApiError(400, "name is required");
+    }
+    if (!email) {
+      throw new ApiError(400, "email is required");
+    }
+    if (!password) {
+      throw new ApiError(400, "password is required");
+    }
+    if (!phone) {
+      throw new ApiError(400, "phone is required");
+    }
+    if (!address) {
+      throw new ApiError(400, "address is required");
+    }
+
+    //check existing user
+    const existingUser = await userModel.findOne({ email });
+    if (existingUser) {
+      return res.status(200).send({
+          success: true,
+          message: "Already registered please login",
+        });
+    }
+
+    //register user
+    const hashedPass = await hashedPassword(password);
+
+    //save
+    const user = await new userModel({
+      name,
+      email,
+      phone,
+      address,
+      password: hashedPass,
+    }).save();
+    res.status(201).send({
+      success: true,
+      message: "User registered successfully",
+      user,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error in Registration",
+      error,
+    });
+  }
+});
--- backend/helpers/authHelper.js
+++ backend/helpers/authHelper.js
@@ -1,8 +1,8 @@
 import bcrypt from 'bcrypt'
-import { asyncHandler } from '../utils/asyncHandler'
-import { ApiError } from '../utils/apiError'
+//import { asyncHandler } from '../utils/asyncHandler'
+import { ApiError } from "../utils/apiError.js";
 
-export const hashPassword = asyncHandler( async(password) => {
+export const hashedPassword = async(password) => {
     try{
         const saltRounds = 10;
         const hashedPassword = await bcrypt.hash(password, saltRounds);
@@ -11,8 +11,13 @@ export const hashPassword = asyncHandler( async(password) => {
         console.log(error)
         throw new ApiError(401, "Password not Hashed properly")
     }
-});
+};
 
-export const comparePassword = asyncHandler( async (password,hashedPassword) => {
-    return bcrypt.compare(password,hashPassword);
-})
+export const comparePassword =  async (password,hashedPassword) => {
+   try {
+    return await bcrypt.compare(password, hashedPassword);
+  } catch (error) {
+    console.error("Compare password error:", error);
+    throw new ApiError(500, "Error comparing passwords");
+  }
+}
