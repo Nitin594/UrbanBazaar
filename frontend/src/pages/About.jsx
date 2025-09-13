267c2395348ff56a53009ab3fbade7fab1e53efb
Nitin594 <nitinmaurya594@gmail.com>
2025-09-13 14:55:10 +0530

    Setup the frontend and create basic routes

---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../components/Layout/Layout";
+const About = () => {
+  return (
+    <div>
+      <Layout>
+        <h1>AboutPage</h1>
+      </Layout>
+    </div>
+  );
+};
+
+export default About;
---
+++ frontend/src/pages/Contact.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../components/Layout/Layout";
+const Contact = () => {
+  return (
+    <div>
+      <Layout>
+        <h1>ContactPage</h1>
+      </Layout>
+    </div>
+  );
+};
+
+export default Contact;
